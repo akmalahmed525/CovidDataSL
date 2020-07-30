c5c8955db1e6c722a230de344c043e76feae3b7e
Ahmed Akmal <akmal.ahmed525@gmail.com>
2020-07-30 20:03:46 +0530

    added another test case

--- src/views/screens/CurrentDataScreen.js
+++ src/views/screens/CurrentDataScreen.js
@@ -127,129 +127,137 @@ type Props = {
 class CurrentDataScreenView extends Component<Props> {
   render() {
     const {locale, data} = this.props;
+
+    const isValuesValid =
+      Object.values(data).filter((u) => typeof u === 'number' && u < 0)
+        .length <= 0;
+
     return (
       <View style={styles.container}>
-        <ScrollView>
-          <View style={styles.banner}>
-            <Text style={[human.title1, styles.title]}>
-              {t('placeholders.title', locale)}
-            </Text>
-            <LottieView
-              style={styles.animationStyle}
-              source={require('assets/animations/17977-corona-virus.json')}
-              autoPlay
-              loop
-            />
-            <Text style={[human.body, styles.textLight, {textAlign: 'center'}]}>
-              {t(`placeholders.data_fetch_info`, locale)}
-            </Text>
-            <OpenURLButton
-              url={Config.API_URL}
-              style={[
-                human.body,
-                styles.text,
-                {textAlign: 'center', color: themes.colors.blue}
-              ]}>
-              {Config.API_URL}
-            </OpenURLButton>
-          </View>
-          <View style={styles.header}>
-            <Text style={[human.title2, styles.text]}>
-              {t(
-                `placeholders.local_covid_cases`,
-                locale,
-                data.update_date_time
-              )}
-            </Text>
-          </View>
-          <View>
-            <ScrollView
-              style={styles.scrollView}
-              horizontal
-              showsHorizontalScrollIndicator={false}>
-              <CardItem
-                title={t('current_data.local_new_cases', locale)}
-                value={data.local_new_cases}
-                valueColor={themes.colors.danger}
+        {isValuesValid && (
+          <ScrollView>
+            <View style={styles.banner}>
+              <Text style={[human.title1, styles.title]}>
+                {t('placeholders.title', locale)}
+              </Text>
+              <LottieView
+                style={styles.animationStyle}
+                source={require('assets/animations/17977-corona-virus.json')}
+                autoPlay
+                loop
               />
-              <CardItem
-                title={t('current_data.local_total_cases', locale)}
-                value={data.local_total_cases}
-                valueColor={themes.colors.danger}
-              />
-              <CardItem
-                title={t(
-                  'current_data.local_total_number_of_individuals_in_hospitals',
-                  locale
+              <Text
+                style={[human.body, styles.textLight, {textAlign: 'center'}]}>
+                {t(`placeholders.data_fetch_info`, locale)}
+              </Text>
+              <OpenURLButton
+                url={Config.API_URL}
+                style={[
+                  human.body,
+                  styles.text,
+                  {textAlign: 'center', color: themes.colors.blue}
+                ]}>
+                {Config.API_URL}
+              </OpenURLButton>
+            </View>
+            <View style={styles.header}>
+              <Text style={[human.title2, styles.text]}>
+                {t(
+                  `placeholders.local_covid_cases`,
+                  locale,
+                  data.update_date_time
                 )}
-                value={data.local_total_number_of_individuals_in_hospitals}
-                valueColor={themes.colors.danger}
-              />
-              <CardItem
-                title={t('current_data.local_deaths', locale)}
-                value={data.local_deaths}
-                valueColor={themes.colors.danger}
-              />
-              <CardItem
-                title={t('current_data.local_new_deaths', locale)}
-                value={data.local_new_deaths}
-                valueColor={themes.colors.danger}
-              />
-              <CardItem
-                title={t('current_data.local_recovered', locale)}
-                value={data.local_recovered}
-                valueColor={themes.colors.green}
-              />
-              <CardItem
-                title={t('current_data.local_active_cases', locale)}
-                value={data.local_active_cases}
-                valueColor={themes.colors.blue}
-              />
-            </ScrollView>
-          </View>
-          <View style={styles.header}>
-            <Text style={[human.title2, styles.text]}>
-              {t(
-                `placeholders.global_covid_cases`,
-                locale,
-                data.update_date_time
-              )}
-            </Text>
-          </View>
-          <View>
-            <ScrollView
-              style={styles.scrollView}
-              horizontal
-              showsHorizontalScrollIndicator={false}>
-              <CardItem
-                title={t('current_data.global_new_cases', locale)}
-                value={data.global_new_cases}
-                valueColor={themes.colors.danger}
-              />
-              <CardItem
-                title={t('current_data.global_total_cases', locale)}
-                value={data.global_total_cases}
-                valueColor={themes.colors.danger}
-              />
-              <CardItem
-                title={t('current_data.global_deaths', locale)}
-                value={data.global_deaths}
-                valueColor={themes.colors.danger}
-              />
-              <CardItem
-                title={t('current_data.global_new_deaths', locale)}
-                value={data.global_new_deaths}
-                valueColor={themes.colors.danger}
-              />
-              <CardItem
-                title={t('current_data.global_recovered', locale)}
-                value={data.global_recovered}
-                valueColor={themes.colors.green}
-              />
-            </ScrollView>
-            <View style={styles.header} />
-          </View>
-        </ScrollView>
+              </Text>
+            </View>
+            <View>
+              <ScrollView
+                style={styles.scrollView}
+                horizontal
+                showsHorizontalScrollIndicator={false}>
+                <CardItem
+                  title={t('current_data.local_new_cases', locale)}
+                  value={data.local_new_cases}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t('current_data.local_total_cases', locale)}
+                  value={data.local_total_cases}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t(
+                    'current_data.local_total_number_of_individuals_in_hospitals',
+                    locale
+                  )}
+                  value={data.local_total_number_of_individuals_in_hospitals}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t('current_data.local_deaths', locale)}
+                  value={data.local_deaths}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t('current_data.local_new_deaths', locale)}
+                  value={data.local_new_deaths}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t('current_data.local_recovered', locale)}
+                  value={data.local_recovered}
+                  valueColor={themes.colors.green}
+                />
+                <CardItem
+                  title={t('current_data.local_active_cases', locale)}
+                  value={data.local_active_cases}
+                  valueColor={themes.colors.blue}
+                />
+              </ScrollView>
+            </View>
+            <View style={styles.header}>
+              <Text style={[human.title2, styles.text]}>
+                {t(
+                  `placeholders.global_covid_cases`,
+                  locale,
+                  data.update_date_time
+                )}
+              </Text>
+            </View>
+            <View>
+              <ScrollView
+                style={styles.scrollView}
+                horizontal
+                showsHorizontalScrollIndicator={false}>
+                <CardItem
+                  title={t('current_data.global_new_cases', locale)}
+                  value={data.global_new_cases}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t('current_data.global_total_cases', locale)}
+                  value={data.global_total_cases}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t('current_data.global_deaths', locale)}
+                  value={data.global_deaths}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t('current_data.global_new_deaths', locale)}
+                  value={data.global_new_deaths}
+                  valueColor={themes.colors.danger}
+                />
+                <CardItem
+                  title={t('current_data.global_recovered', locale)}
+                  value={data.global_recovered}
+                  valueColor={themes.colors.green}
+                />
+              </ScrollView>
+              <View style={styles.header} />
+            </View>
+          </ScrollView>
+        )}
       </View>
     );
   }
