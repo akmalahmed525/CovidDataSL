1332fc4b1ee7ae7a529b7fe615b4499f04584383
Ahmed Akmal <akmal.ahmed525@gmail.com>
2020-07-30 19:17:42 +0530

    Added a test case to i18n module

--- __tests__/src/i18n/index.test.js
+++ __tests__/src/i18n/index.test.js
@@ -1,7 +1,11 @@
 import {t} from 'i18n';
 
 describe('Must translate the language', () => {
-  it('should translate the language', () => {
+  it('should translate the language - correct one', () => {
     expect(t('about.title')).toBe('Covid-19 Data Visualization Application');
+  });
+
+  it('should translate the language - wrong one', () => {
+    expect(t('about.title2')).not.toBe('Covid-19 Data Visualization Application');
   });
 });
